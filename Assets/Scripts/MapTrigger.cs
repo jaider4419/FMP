eb782d57cb85f9f41757158f1bf817a623b3e568
jaider4419 <jaiderboateng8@gmail.com>
2024-03-19 12:46:33 +0000

    ad

--- Assets/Scripts/MapTrigger.cs
+++ Assets/Scripts/MapTrigger.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MapTrigger : MonoBehaviour
 {
+    public GameObject mapUI;
+    private bool mapShowing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mapUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mapUI.SetActive(true);
+            mapShowing = true;
+        }
     }
 }
