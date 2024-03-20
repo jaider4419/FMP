5cf9ee5193a95159a6f4293cbfe7d6dacb41f29e
jaider4419 <jaiderboateng8@gmail.com>
2024-03-20 10:13:02 +0000

    done map config

--- Assets/Scripts/MapTrigger.cs
+++ Assets/Scripts/MapTrigger.cs
@@ -1,44 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MapTrigger : MonoBehaviour
+public class ToggleUI : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Map;
-    bool isShowing;
+
+    private bool uiVisible = false;
 
     void Start()
     {
-        Map.SetActive(false);
+        // Ensure the UI element is initially hidden
+        if (Map != null)
+            Map.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M))
         {
-            if (isShowing)
-            {
-                ShowtheMap();
-            }
-            else
-            {
-                DontShow();
-            }
+            uiVisible = !uiVisible;
+            if (Map != null)
+                Map.SetActive(uiVisible);
         }
-
-    }
-
-    public void ShowtheMap()
-    {
-        isShowing=true;
-        Map.SetActive(true);
-    }
-
-    public void DontShow()
-    {
-        isShowing=false;
-        Map.SetActive(false);
     }
 }
