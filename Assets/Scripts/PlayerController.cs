d409bcb1cf5e9ae07234c1b72f030053ec01d11b
jaider4419 <jaiderboateng8@gmail.com>
2024-03-21 12:43:32 +0000

    stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,9 +28,13 @@ public class NewBehaviourScript : MonoBehaviour
         moveInput.y = Input.GetAxis("Vertical");
         moveInput.Normalize();
 
+        Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0f) * moveSpeed * Time.deltaTime;
+
         theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
 
         anim.SetFloat("moveSpeed", theRB.velocity.magnitude);
+
+        transform.Translate(movement);
 
         RaycastHit hit;
         if (Physics.Raycast(groundPoint.position, Vector3.down, out hit, .3f, whatIsGround))
@@ -46,6 +50,47 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             theRB.velocity += new Vector3(0f, jumpForce, 0f);
+        }
+
+        if (moveInput.x > 0)
+        {
+            Debug.Log("Moving Right");
+            anim.SetBool("IsWalkingLeft", false);
+            anim.SetBool("IsIdle", true);
+        }
+        else if (moveInput.x < 0)
+        {
+            Debug.Log("Moving Left");
+            anim.SetBool("IsWalkingLeft", true);
+        }
+
+        if (moveInput.y > 0)
+        {
+            Debug.Log("Moving Up");
+        }
+        else if (moveInput.y < 0)
+        {
+            Debug.Log("Moving Down");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.Play("Idle");
+        }
+    }
+
+    public void CheckMovement()
+    {
+        float currentY = transform.position.y;
+        float lastY = currentY;
+
+        if (currentY > lastY)
+        {
+            Debug.Log("going down");
+        }
+        else
+        {
+            Debug.Log("going up");
         }
     }
 }
