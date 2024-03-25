5b0eedb13203613a0fc8f710d619f5fe3d462341
jaider4419 <jaiderboateng8@gmail.com>
2024-03-25 16:46:00 +0000

    chrissy waike up

--- Assets/SpriteBillboard.cs
+++ Assets/SpriteBillboard.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
     }
 }
