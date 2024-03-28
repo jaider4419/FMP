587e359f67197a860dfdcce1f96a541c5cce2415
jaider4419 <jaiderboateng8@gmail.com>
2024-03-28 12:43:35 +0000

    saved

--- Assets/SpriteBillboard.cs
+++ Assets/SpriteBillboard.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
-
-    public Camera maincam;
-
     public void LateUpdate()
     {
         transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
